6bd384720cc134775c9d309b6bfe419568784cd3
Dayana Ilieva <dayana.ilieva@1forfit.com>
2023-08-28 17:05:30 +0300

    move input markup into chatbot

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -3,10 +3,13 @@ import ChatWrapper from './components/ChatWrapper';
 import Head from './components/Head';
 import MessagesWrapper from './components/MessagesWrapper';
 import InitiatorProfile from './components/InitiatorProfile';
+import SendButton from './components/SendButton';
 import { assistant } from './lib/config/assistant';
+import { translations } from './lib/config/translations';
+
 import "./styles/index.css";
 
-const Chatbot = (props) => {
+const Chatbot = ({ }) => {
   return (
     <ChatWrapper>
       <Head assistant={assistant} />
@@ -22,7 +25,27 @@ const Chatbot = (props) => {
           </div>
         </span>
       </MessagesWrapper>
-    </ChatWrapper>
+
+      {/* <a class="chat-widget__cta hidden" id="cta-button">${config.assistant.ctaTextContent}</a>
+      ${paymentButton}
+      ${loadingDots}
+      ${chatPaymentFormContainer(config.translations)} */}
+      <div>
+        <div class="js-error error-message hidden">{translations.error}</div>
+        <div class="js-error-email error-message hidden"></div>
+        <div id="error-label" class="error-message hidden"></div>
+        <div class="chat-widget__prompt" id="prompt-container">
+          <span class="widget__input">
+            <input id="chat-email" class="hidden" autofocus="chat" name="email" type="email" placeholder={translations.emailPlaceholder} />
+            <input id="chat-prompt" minlength="1" autofocus="chat" name="chat-prompt" type="text" placeholder={translations.textareaPlaceholder} />
+            <span class="animate-spin hidden js-email-processing">
+              <span class="spin-icon"></span>
+            </span>
+          </span>
+          <SendButton />
+        </div>
+      </div>
+    </ChatWrapper >
   );
 };
 
